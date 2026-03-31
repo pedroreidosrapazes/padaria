3bad5519db26a6022d4a96b7026ff921882ef770
pedroreidosrapazes <pepedrito08.09@gmail.com>
2026-03-31 01:17:40 -0300

    fiz algumas alterações na parte do css

---
+++ padaria/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace padaria
+{
+    class User
+    {
+        public string name;
+        public string email;
+        public string number;
+        public string cpf;
+    }
+}
--- padaria/views/NewAcount.xaml.cs
+++ padaria/views/NewAcount.xaml.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace padaria.views; 
 
 public partial class NewAcount : ContentPage
@@ -5,5 +7,34 @@ public partial class NewAcount : ContentPage
     public NewAcount()
     {
         InitializeComponent();
+    }
+
+    List<User> ListUsers = new List<User>();
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(NameUser.Text) ||
+            string.IsNullOrWhiteSpace(CPF.Text) ||
+            string.IsNullOrWhiteSpace(Number.Text) ||
+            string.IsNullOrWhiteSpace(EmailUser.Text))
+        {
+
+            await DisplayAlert("WhiteSpace", "preencha todos os campos", "ok");
+            return;
+        }
+
+        
+            User newUser = new User();
+
+            newUser.name = NameUser.Text;
+            newUser.number = Number.Text;
+            newUser.cpf = CPF.Text;
+            newUser.email = EmailUser.Text;
+
+            ListUsers.Add(newUser);
+
+
+
+        
+
     }
 }
